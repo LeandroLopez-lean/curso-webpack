d7d894b8a8658bff091d9fb3ca715f1f9c9f556a
Leandro <leandrolopez131982@gmail.com>
2023-11-03 11:36:19 -0300

    index base y recurso de indexjs

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+
+const data = {
+    name: "Lean Lopez",
+    nickname: "Lean",
+    description: "...",
+    avatar: "...",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/",
+        username: "",
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/",
+        username: "",
+      },
+    ],
+    links: [
+      {
+        name: "blog",
+        url: "https://example.com/",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://example.com/",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Argentina",
+  };
