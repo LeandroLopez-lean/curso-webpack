7fdc2f95a802c9684e6d36e84ce93b222d44d2d5
Leandro <leandrolopez131982@gmail.com>
2023-11-09 10:07:04 -0300

    install webpack-dev-server -D

--- src/index.js
+++ src/index.js
@@ -37,14 +37,16 @@ const data = {
 
 const main = () => {
     let name = document.createTextNode(data?.name);
-    let links = data?.links?.map((network) => {
-        return `<div class="bg-yellow-200 px-4 py-5 w-full flex justify-between">
-        <a class="text-sm font-bold text-yellow-600 text-center hhover:text-yellow-800 cursor-pointer"
-        href="https://gndx.dev/" target="blank">
-        BLOG</a><span>librito</span></div>`;
+    let links = data?.links?.map((link) => {
+        return `<div class="bg-${link.color}-200 px-4 py-5 w-full flex justify-between">
+        <a class="text-sm font-bold text-${link.color}-600 text-center hhover:text-${link.color}-800 cursor-pointer"
+        href="https:${link.url}" target="blank">
+        ${link.name}</a><span>librito</span></div>`;
     }).join('');
     let newItem = document.createElement("section");
     newItem.innerHTML = links;
     $links.appendChild(newItem);
     $name.appendChild(name);
 }
+
+main();
